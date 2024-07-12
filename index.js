20caddca8d882bc6de226a34a3463675469bbc46
joel_patrick <jayjoelpn@gmail.com>
2024-07-13 00:01:58 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+// Define cats array with initial values
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Destructively append a cat to the end of the cats array
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// Destructively prepend a cat to the beginning of the cats array
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+// Destructively remove the last cat from the cats array
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// Destructively remove the first cat from the cats array
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Append a cat to the cats array and return a new array
+function appendCat(name) {
+  return [...cats, name];
+}
+
+// Prepend a cat to the cats array and return a new array
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+// Remove the last cat in the cats array and return a new array
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+// Remove the first cat from the cats array and return a new array
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
